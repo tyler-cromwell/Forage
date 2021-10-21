533bd72953dc2588c3d8cf2353fb7ed6e674b3b8
Tyler Cromwell <tjc6185@gmail.com>
2021-10-20 21:32:37 -0400

    Added Twilio client

---
+++ clients/twilio.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"os"
+
+	"github.com/twilio/twilio-go"
+	openapi "github.com/twilio/twilio-go/rest/api/v2010"
+)
+
+var twilioClient *twilio.RestClient
+
+func init() {
+	// Get Twilio credentials
+	accountSid := os.Getenv("TWILIO_ACCOUNT_SID")
+	authToken := os.Getenv("TWILIO_AUTH_TOKEN")
+
+	// Initialize Twilio client
+	twilioClient = twilio.NewRestClientWithParams(twilio.RestClientParams{
+		Username: accountSid,
+		Password: authToken,
+	})
+}
+
+func SendMessage(phoneFrom, phoneTo, message string) (string, error) {
+	// Prepare message
+	params := &openapi.CreateMessageParams{}
+	params.SetFrom(phoneFrom)
+	params.SetTo(phoneTo)
+	params.SetBody(message)
+
+	// Send it
+	resp, err := twilioClient.ApiV2010.CreateMessage(params)
+	if err != nil {
+		return "", err
+	} else {
+		return *resp.Sid, err
+	}
+}
--- go.mod
+++ go.mod
@@ -8,10 +8,10 @@ require (
 	github.com/gorilla/mux v1.8.0
 	github.com/klauspost/compress v1.13.5 // indirect
 	github.com/sirupsen/logrus v1.8.1
+	github.com/twilio/twilio-go v0.17.0 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
 	go.mongodb.org/mongo-driver v1.7.2
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20210910150752-751e447fb3d0 // indirect
 	golang.org/x/text v0.3.7 // indirect
 )

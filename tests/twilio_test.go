637b31f3d30e8e0d47ee0eb4c871551d5ea10547
Tyler Cromwell <tjc6185@gmail.com>
2021-12-21 19:29:50 -0500

    Added unit test for ComposeMessage

--- tests/twilio_test.go
+++ tests/twilio_test.go
@@ -1,16 +1,42 @@
 package tests
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	"github.com/tyler-cromwell/forage/clients"
+	"github.com/tyler-cromwell/forage/tests/mocks"
 )
 
 func TestTwilioClient(t *testing.T) {
 	t.Run("NewTwilioClientWrapper", func(t *testing.T) {
-		// Case: "Success"
 		client := clients.NewTwilioClientWrapper("", "", "", "")
 		require.NotNil(t, client)
+	})
+
+	t.Run("ComposeMessage", func(t *testing.T) {
+		client := mocks.NewTwilioClientWrapper("", "", "", "")
+		require.NotNil(t, client)
+
+		cases := []struct {
+			quantity        int
+			quantityExpired int
+			url             string
+			want            string
+		}{
+			{1, 0, "http://nothing.com", fmt.Sprintf("%d item expiring soon and %d already expired! View shopping list: %s", 1, 0, "http://nothing.com")},
+			{2, 0, "http://nothing.com", fmt.Sprintf("%d items expiring soon and %d already expired! View shopping list: %s", 2, 0, "http://nothing.com")},
+			{0, 1, "http://nothing.com", fmt.Sprintf("%d item expired! View shopping list: %s", 1, "http://nothing.com")},
+			{0, 2, "http://nothing.com", fmt.Sprintf("%d items expired! View shopping list: %s", 2, "http://nothing.com")},
+			{0, 0, "http://nothing.com", ""},
+		}
+
+		for _, c := range cases {
+			got := client.ComposeMessage(c.quantity, c.quantityExpired, c.url)
+			if got != c.want {
+				t.Errorf("ComposeMessage(%d, %d, \"%s\"), got (\"%s\"), want (\"%s\")", c.quantity, c.quantityExpired, c.url, got, c.want)
+			}
+		}
 	})
 }

b44f2c33b1dd709465ca4e9179b9b39237d93b35
Tyler Cromwell <tjc6185@gmail.com>
2022-05-10 22:30:30 -0400

    Created an interface layer for mocking the MongoDB client

--- config/config.go
+++ config/config.go
@@ -1,11 +1,24 @@
 package config
 
 import (
+	"context"
 	"time"
 
 	"github.com/sirupsen/logrus"
 	"github.com/tyler-cromwell/forage/clients"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+type MongoHandle interface {
+	FindOneDocument(context.Context, bson.D) (*bson.M, error)
+	FindDocuments(context.Context, bson.M, *options.FindOptions) ([]bson.M, error)
+	InsertOneDocument(context.Context, interface{}) error
+	InsertManyDocuments(context.Context, []interface{}) error
+	UpdateOneDocument(context.Context, bson.D, interface{}) (int64, int64, error)
+	DeleteOneDocument(context.Context, bson.D) error
+	DeleteManyDocuments(context.Context, bson.M) (int64, error)
+}
 
 type Configuration struct {
 	ContextTimeout time.Duration
@@ -13,7 +26,7 @@ type Configuration struct {
 	Lookahead      time.Duration
 	LogrusLevel    logrus.Level
 	ListenSocket   string
-	Mongo          *clients.Mongo
+	Mongo          MongoHandle //*clients.Mongo
 	Trello         *clients.Trello
 	Twilio         *clients.Twilio
 }
